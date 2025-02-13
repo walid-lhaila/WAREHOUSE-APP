88a234da4095695ddbcac3b0a82f2b77983556ce
walid lhaila <walidlhaila00@gmail.com>
2025-02-13 22:17:07 +0100

    Feat: IMPLEMENATION TO SEARCH BY NAME AND TYPE ALSO PRICE

--- app/Components/Search.tsx
+++ app/Components/Search.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import {Pressable, ScrollView, Text, TextInput, View} from "react-native";
+import React, {useEffect, useState} from 'react';
+import {FlatList, Pressable, Text, TextInput, View} from "react-native";
 import {Ionicons} from "@expo/vector-icons";
 import ProductsCard from "@/app/Components/ProductsCard";
-import nido from "../../assets/images/nido.png"
+import {useDispatch, useSelector} from "react-redux";
+
 
 function Search({ close }) {
+    const dispatch = useDispatch();
+    const {products} = useSelector((state) => state.products);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [filteredProducts, setFilteredProducts] = useState(products);
+    const [typingTimeout, setTypingTimeout] = useState(null);
+
+
+    useEffect(() => {
+        if (typingTimeout) {
+            clearTimeout(typingTimeout);
+        }
+
+        const timeout = setTimeout(() => {
+            const filtered = products.filter(product => {
+                const searchString = searchQuery.toLowerCase();
+                return (
+                    product.name.toLowerCase().includes(searchString) ||
+                    product.type.toLowerCase().includes(searchString) ||
+                    product.price.toString().includes(searchQuery)
+                );
+            });
+            setFilteredProducts(filtered);
+        }, 100);
+
+        setTypingTimeout(timeout);
+
+        return () => {
+            if (typingTimeout) {
+                clearTimeout(typingTimeout);
+            }
+        };
+    }, [searchQuery]);
+
     return (
         <View>
             <View style={{height: '100%', width: '100%'}}>
@@ -15,12 +49,22 @@ function Search({ close }) {
                 </View>
                 <View style={{backgroundColor: 'white', width: '95%', marginHorizontal: 'auto', marginTop: 20, borderRadius: 20, height: 50, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: 10}}>
                     <Ionicons name={"search-sharp"} color={'#888B90'} size={32} />
-                    <TextInput style={{width: '76%', fontSize: 17, fontFamily: 'mono', color: 'white'}} keyboardType="default" placeholder="Search Products..."  placeholderTextColor="#888B90" />
+                    <TextInput style={{width: '76%', fontSize: 17, fontFamily: 'mono', color: 'black'}} keyboardType="default" placeholder="Search Products..." value={searchQuery} onChangeText={setSearchQuery}  placeholderTextColor="#888B90" />
                     <Ionicons name={'mic-sharp'} color={'#888B90'} size={32} />
                 </View>
-                <ScrollView contentContainerStyle={{ paddingTop: 20, paddingHorizontal: 5 }}>
-                    <ProductsCard type={"Nutrition"} price={1200} src={nido} name={"Nido"} quantity={100} />
-                </ScrollView>
+                <FlatList data={filteredProducts}  keyExtractor={(item) => item.id} numColumns={2}  renderItem={({ item }) => (
+                    <View style={{paddingHorizontal: 5, paddingVertical: 5, marginTop: 10}}>
+                        <ProductsCard type={item.type} price={item.price} src={item.image} name={item.name} quantity={item.solde} />
+                    </View>
+                    )}
+                      ListEmptyComponent={
+                          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: 50 }}>
+                              <Text style={{ fontSize: 18, color: 'gray' }}>
+                                  No products found
+                              </Text>
+                          </View>
+                      }
+                />
             </View>
         </View>
     );
